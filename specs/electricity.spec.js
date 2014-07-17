3d4ce40e0f3780bebbb6540fdd105d7b710a5c4a
Benjamin Polidore <polidore@gmail.com>
2014-07-17 08:09:37 -0400

    scaffolding

---
+++ specs/electricity.spec.js
@@ -0,0 +1,10 @@
+var dfg = '../lib';
+
+describe("Electricity", function() {
+  it("calls my tests", function() {
+    expect(true).toBe(true);
+  });
+  it("Loads dfg", function() {
+    expect(dfg).toBeTruthy();
+  });
+});
