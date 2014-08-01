a6e44e7cd7cfbe2c6cdc90ae8305ecf7fc720436
Ben Polidore <ben.polidore@itg.com>
2014-08-01 14:58:47 -0400

    remove some logging

--- specs/fileBased.spec.js
+++ specs/fileBased.spec.js
@@ -3,7 +3,7 @@ var loadJsons = require('../lib/loadJsons');
 describe("Get a recursive directory load of JSONs", function() {
   it("Should return a non-null object", function(done) {
     loadJsons("./specs")(function(json) {
-      expect(json).toBeTruthy();
+      expect(json.length).toBe(3);
       done();
     });
   });
