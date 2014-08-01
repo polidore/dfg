21bb2045d1df9ec2067be5273e44465799b60a3c
Ben Polidore <ben.polidore@itg.com>
2014-08-01 15:57:06 -0400

    better use of Q

--- specs/fileBased.spec.js
+++ specs/fileBased.spec.js
@@ -1,10 +1,34 @@
 var loadJsons = require('../lib/loadJsons');
 
 describe("Get a recursive directory load of JSONs", function() {
-  it("Should return a non-null object", function(done) {
-    loadJsons("./specs")(function(json) {
-      expect(json.length).toBe(3);
-      done();
+  var data;
+  beforeEach(function(done) {
+    if(data) done();
+    else {
+      loadJsons("./specs")(function(d) {
+        data = d;
+        done();
+      });
+    }
+  });
+
+  it("Should return right number of jsons", function() {
+    expect(data.length).toBe(3);
+  });
+
+  it("Should have a @type field on all objects", function() {
+    data.forEach(function(d) {
+      expect(d['@type']).toBeDefined();
     });
+  });
+
+  it("Should have an @override field on all but 1 object", function() {
+    var total = 0;
+    var numWithO = 0;
+    for(var i in data) {
+      total++;
+      if(data[i]['@override']) numWithO++;
+    }
+    expect(total-numWithO).toBe(1);
   });
 });
