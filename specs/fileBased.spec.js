7763fabdcba3a31a67a5c3f40748898ec7656030
Ben Polidore <ben.polidore@itg.com>
2014-08-01 14:53:20 -0400

    it does stuff

---
+++ specs/fileBased.spec.js
@@ -0,0 +1,10 @@
+var loadJsons = require('../lib/loadJsons');
+
+describe("Get a recursive directory load of JSONs", function() {
+  it("Should return a non-null object", function(done) {
+    loadJsons("./specs")(function(json) {
+      expect(json).toBeTruthy();
+      done();
+    });
+  });
+});
