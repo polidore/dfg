19f763c5a7f8b78b4058612a6bf22fc9c257cea2
Ben Polidore <ben.polidore@itg.com>
2014-08-15 15:54:48 -0400

    this commit doesn't work, but it will soon.

--- specs/dfg.spec.js
+++ specs/dfg.spec.js
@@ -1,5 +1,15 @@
 var DFG = require('../');
 
+/*
+[ { '@type': 'electricity', kwhRate: 0.2, ac: true },
+  { '@type': 'electricity',
+    '@override': { country: 'US', state: 'NY' },
+    kwhRate: 0.19 },
+  { '@type': 'electricity',
+    '@override': { country: 'US' },
+    kwhRate: 0.12 } ]
+*/
+
 describe("DFG lets you Do it with conFiGs!", function() {
   var dfg;
   beforeEach(function(done) {
@@ -15,7 +25,12 @@ describe("DFG lets you Do it with conFiGs!", function() {
     expect(dfg._data.length).toBe(3);
   });
   it("Should let me create a type", function() {
-    dfg.createType('electricity',[function city() { return 'NY'}]);
+    dfg.createType('electricity',['country','state','city']);
     expect(dfg._types['electricity']).toBeTruthy();
+  });
+  it("Should return the defaults when lacking a context", function() {
+    var cfg = dfg.getCfg('electricity');
+    expect(cfg.kwhRate).toBe(0.2);
+    expect(cfg.ac).toBeTruthy();
   });
 });
