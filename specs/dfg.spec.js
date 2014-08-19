f47b8ae6e9a5e5b64dda296193eb013b12a0ce8d
Ben Polidore <polidore@gmail.com>
2014-08-19 18:31:42 -0400

    more rigorous testing

--- specs/dfg.spec.js
+++ specs/dfg.spec.js
@@ -20,6 +20,7 @@ describe("DFG lets you Do it with conFiGs!", function() {
   });
   it("Should return the defaults when lacking a context", function() {
     var cfg = dfg.getCfg('electricity');
+    expect(cfg.voltage).toBe(220);
     expect(cfg.kwhRate).toBe(0.2);
     expect(cfg.ac).toBeTruthy();
   });
@@ -27,35 +28,41 @@ describe("DFG lets you Do it with conFiGs!", function() {
     var cfg = dfg.getCfg('electricity', {country:'US'});
     expect(cfg['@cacheMiss']).toBeTruthy();
     expect(cfg.kwhRate).toBe(0.12);
+    expect(cfg.voltage).toBe(110);
     expect(cfg.ac).toBeTruthy();
   });
   it("Should return $0.12 when country is US and state is NJ", function() {
     var cfg = dfg.getCfg('electricity', {country:'US',state:'NJ'});
     expect(cfg['@secondaryCache']).toBeTruthy();
     expect(cfg.kwhRate).toBe(0.12);
+    expect(cfg.voltage).toBe(110);
     expect(cfg.ac).toBeTruthy();
   });
   it("Should return $0.19 when country is US and state is NY", function() {
     var cfg = dfg.getCfg('electricity', {country:'US',state:'NY'});
     expect(cfg['@cacheMiss']).toBeTruthy();
     expect(cfg.kwhRate).toBe(0.19);
+    expect(cfg.voltage).toBe(110);
     expect(cfg.ac).toBeTruthy();
   });
-  it("Should return $0.19 when state is NY", function() {
+  it("Should return $0.19 when state is NY and country is null", function() {
     var cfg = dfg.getCfg('electricity', {state:'NY'});
     expect(cfg['@cacheMiss']).toBeTruthy();
     expect(cfg.kwhRate).toBe(0.19);
+    expect(cfg.voltage).toBe(220);
     expect(cfg.ac).toBeTruthy();
   });
   it("Should return $0.2 when state is NJ and country is null", function() {
     var cfg = dfg.getCfg('electricity', {state:'NJ'});
     expect(cfg['@cacheMiss']).toBeTruthy();
     expect(cfg.kwhRate).toBe(0.2);
+    expect(cfg.voltage).toBe(220);
     expect(cfg.ac).toBeTruthy();
   });
-  it("Should return hydro 0.2 when counry is CA", function() {
-    var cfg = dfg.getCfg('electricity', {country:'CA'});
+  it("Should return voltage 220 when counry is UK", function() {
+    var cfg = dfg.getCfg('electricity', {country:'UK'});
     expect(cfg['@secondaryCache']).toBeTruthy();
+    expect(cfg.voltage).toBe(220);
     expect(cfg.hydro).toBe(0.2);
     expect(cfg.ac).toBeTruthy();
   });
