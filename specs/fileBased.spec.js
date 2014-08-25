0b2c089a4484d3540c16583c449318eee219bc60
Ben Polidore <ben.polidore@itg.com>
2014-08-25 16:21:43 -0400

    Must run dupe check when a type is created

--- specs/fileBased.spec.js
+++ specs/fileBased.spec.js
@@ -13,22 +13,12 @@ describe("Get a recursive directory load of JSONs", function() {
   });
 
   it("Should return right number of jsons", function() {
-    expect(data.length).toBe(3);
+    expect(data.length).toBe(6);
   });
 
   it("Should have a @type field on all objects", function() {
     data.forEach(function(d) {
       expect(d['@type']).toBeDefined();
     });
-  });
-
-  it("Should have an @override field on all but 1 object", function() {
-    var total = 0;
-    var numWithO = 0;
-    for(var i in data) {
-      total++;
-      if(data[i]['@override']) numWithO++;
-    }
-    expect(total-numWithO).toBe(1);
   });
 });
